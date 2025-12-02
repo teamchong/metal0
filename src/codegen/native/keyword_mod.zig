100c5692affaffc8eac25961582ef5f44d1797f0
teamchong <25894545+teamchong@users.noreply.github.com>
2025-12-02 12:01:06 -0500

    refactor: Simplify keyword, secrets, textwrap modules using h.wrap() helpers

--- src/codegen/native/keyword_mod.zig
+++ src/codegen/native/keyword_mod.zig
@@ -1,27 +1,13 @@
 /// Python keyword module - Test whether strings are Python keywords
 const std = @import("std");
-const ast = @import("ast");
 const h = @import("mod_helper.zig");
-const CodegenError = h.CodegenError;
-const NativeCodegen = h.NativeCodegen;
 
 const kwlist = "\"False\", \"None\", \"True\", \"and\", \"as\", \"assert\", \"async\", \"await\", \"break\", \"class\", \"continue\", \"def\", \"del\", \"elif\", \"else\", \"except\", \"finally\", \"for\", \"from\", \"global\", \"if\", \"import\", \"in\", \"is\", \"lambda\", \"nonlocal\", \"not\", \"or\", \"pass\", \"raise\", \"return\", \"try\", \"while\", \"with\", \"yield\"";
 const softkwlist = "\"_\", \"case\", \"match\", \"type\"";
 
 pub const Funcs = std.StaticStringMap(h.H).initComptime(.{
-    .{ "iskeyword", genIskeyword }, .{ "issoftkeyword", genIssoftkeyword },
+    .{ "iskeyword", h.wrap("blk: { const s = ", "; const keywords = [_][]const u8{ " ++ kwlist ++ " }; for (keywords) |kw| { if (std.mem.eql(u8, s, kw)) break :blk true; } break :blk false; }", "false") },
+    .{ "issoftkeyword", h.wrap("blk: { const s = ", "; const softkw = [_][]const u8{ " ++ softkwlist ++ " }; for (softkw) |kw| { if (std.mem.eql(u8, s, kw)) break :blk true; } break :blk false; }", "false") },
     .{ "kwlist", h.c("metal0_runtime.PyList([]const u8).fromSlice(&[_][]const u8{ " ++ kwlist ++ " })") },
     .{ "softkwlist", h.c("metal0_runtime.PyList([]const u8).fromSlice(&[_][]const u8{ " ++ softkwlist ++ " })") },
 });
-
-fn genIskeyword(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) { try self.emit("false"); return; }
-    try self.emit("blk: { const s = "); try self.genExpr(args[0]);
-    try self.emit("; const keywords = [_][]const u8{ " ++ kwlist ++ " }; for (keywords) |kw| { if (std.mem.eql(u8, s, kw)) break :blk true; } break :blk false; }");
-}
-
-fn genIssoftkeyword(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) { try self.emit("false"); return; }
-    try self.emit("blk: { const s = "); try self.genExpr(args[0]);
-    try self.emit("; const softkw = [_][]const u8{ " ++ softkwlist ++ " }; for (softkw) |kw| { if (std.mem.eql(u8, s, kw)) break :blk true; } break :blk false; }");
-}
--- src/codegen/native/textwrap_mod.zig
+++ src/codegen/native/textwrap_mod.zig
@@ -5,16 +5,19 @@ const h = @import("mod_helper.zig");
 const CodegenError = h.CodegenError;
 const NativeCodegen = h.NativeCodegen;
 
+const dedentBody = "; var _min_indent: usize = std.math.maxInt(usize); var _lines_iter = std.mem.splitSequence(u8, _text, \"\\n\"); while (_lines_iter.next()) |line| { if (line.len == 0) continue; var _spaces: usize = 0; for (line) |c| { if (c == ' ') _spaces += 1 else break; } if (_spaces < _min_indent) _min_indent = _spaces; } if (_min_indent == std.math.maxInt(usize)) _min_indent = 0; var _result = std.ArrayList(u8){}; var _lines_iter2 = std.mem.splitSequence(u8, _text, \"\\n\"); var _first = true; while (_lines_iter2.next()) |line| { if (!_first) _result.append(__global_allocator, '\\n') catch continue; _first = false; if (line.len > _min_indent) _result.appendSlice(__global_allocator, line[_min_indent..]) catch continue; } break :blk _result.items; }";
+
 pub const Funcs = std.StaticStringMap(h.H).initComptime(.{
-    .{ "wrap", genWrap }, .{ "fill", genFill }, .{ "dedent", genDedent },
-    .{ "indent", genIndent }, .{ "shorten", genShorten },
+    .{ "wrap", genWrap }, .{ "fill", genFill },
+    .{ "dedent", h.wrap("blk: { const _text = ", dedentBody, "\"\"") },
+    .{ "indent", genIndent },
+    .{ "shorten", genShorten },
     .{ "TextWrapper", h.c("struct { width: usize = 70, initial_indent: []const u8 = \"\", subsequent_indent: []const u8 = \"\", pub fn wrap(__self: *@This(), text: []const u8) [][]const u8 { _ = __self; _ = text; return &.{}; } pub fn fill(__self: *@This(), text: []const u8) []const u8 { _ = __self; return text; } }{}") },
 });
 
 fn genWidth(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
     try self.emit("; const _width: usize = ");
-    if (args.len > 1) { try self.emit("@intCast("); try self.genExpr(args[1]); try self.emit(")"); }
-    else { try self.emit("70"); }
+    if (args.len > 1) { try self.emit("@intCast("); try self.genExpr(args[1]); try self.emit(")"); } else try self.emit("70");
 }
 
 fn genWrap(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
@@ -27,12 +30,6 @@ fn genFill(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
     if (args.len == 0) return;
     try self.emit("blk: { const _text = "); try self.genExpr(args[0]); try genWidth(self, args);
     try self.emit("; var _result = std.ArrayList(u8){}; var _start: usize = 0; while (_start < _text.len) { const _end = @min(_start + _width, _text.len); if (_start > 0) _result.append(__global_allocator, '\\n') catch continue; _result.appendSlice(__global_allocator, _text[_start.._end]) catch continue; _start = _end; } break :blk _result.items; }");
-}
-
-fn genDedent(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) return;
-    try self.emit("blk: { const _text = "); try self.genExpr(args[0]);
-    try self.emit("; var _min_indent: usize = std.math.maxInt(usize); var _lines_iter = std.mem.splitSequence(u8, _text, \"\\n\"); while (_lines_iter.next()) |line| { if (line.len == 0) continue; var _spaces: usize = 0; for (line) |c| { if (c == ' ') _spaces += 1 else break; } if (_spaces < _min_indent) _min_indent = _spaces; } if (_min_indent == std.math.maxInt(usize)) _min_indent = 0; var _result = std.ArrayList(u8){}; var _lines_iter2 = std.mem.splitSequence(u8, _text, \"\\n\"); var _first = true; while (_lines_iter2.next()) |line| { if (!_first) _result.append(__global_allocator, '\\n') catch continue; _first = false; if (line.len > _min_indent) _result.appendSlice(__global_allocator, line[_min_indent..]) catch continue; } break :blk _result.items; }");
 }
 
 fn genIndent(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
