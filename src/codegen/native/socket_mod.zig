91806c63d56ba7425fb83ed2c27a7d660b083d14
teamchong <25894545+teamchong@users.noreply.github.com>
2025-12-02 12:05:26 -0500

    refactor: Simplify socket, zipfile modules using h.wrap() helpers

--- src/codegen/native/socket_mod.zig
+++ src/codegen/native/socket_mod.zig
@@ -1,42 +1,21 @@
 /// Python socket module - Basic TCP/UDP socket operations
 const std = @import("std");
-const ast = @import("ast");
 const h = @import("mod_helper.zig");
-const CodegenError = h.CodegenError;
-const NativeCodegen = h.NativeCodegen;
 
-fn genByteSwap(comptime pre: []const u8, comptime ty: []const u8, comptime func: []const u8) h.H {
-    return struct { fn f(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-        if (args.len == 0) return;
-        try self.emit(pre ++ "std.mem." ++ func ++ "(" ++ ty ++ ", @intCast("); try self.genExpr(args[0]); try self.emit("))))");
-    } }.f;
-}
+const createConnBody = "; const _host = _addr_tuple.@\"0\"; const _port = _addr_tuple.@\"1\"; const _sock = std.posix.socket(std.posix.AF.INET, std.posix.SOCK.STREAM, 0) catch break :blk @as(i64, -1); var _addr: std.posix.sockaddr.in = .{ .family = std.posix.AF.INET, .port = std.mem.nativeToBig(u16, @intCast(_port)), .addr = blk2: { if (std.mem.eql(u8, _host, \"localhost\") or std.mem.eql(u8, _host, \"127.0.0.1\")) { break :blk2 .{ .s_addr = std.mem.nativeToBig(u32, 0x7f000001) }; } else { break :blk2 .{ .s_addr = 0 }; } }, .zero = [_]u8{0} ** 8 }; std.posix.connect(_sock, @ptrCast(&_addr), @sizeOf(@TypeOf(_addr))) catch break :blk @as(i64, -1); break :blk @as(i64, @intCast(_sock)); }";
+const inetAtonBody = "; var _parts: [4]u8 = undefined; var _iter = std.mem.splitScalar(u8, _ip_str, '.'); var _i: usize = 0; while (_iter.next()) |_part| : (_i += 1) { if (_i >= 4) break; _parts[_i] = std.fmt.parseInt(u8, _part, 10) catch 0; } break :blk __global_allocator.dupe(u8, &_parts) catch \"\"; }";
+const inetNtoaBody = "; if (_packed.len < 4) break :blk \"0.0.0.0\"; var _buf: [16]u8 = undefined; const _len = std.fmt.bufPrint(&_buf, \"{d}.{d}.{d}.{d}\", .{ _packed[0], _packed[1], _packed[2], _packed[3] }) catch break :blk \"0.0.0.0\"; break :blk __global_allocator.dupe(u8, _len) catch \"0.0.0.0\"; }";
 
 pub const Funcs = std.StaticStringMap(h.H).initComptime(.{
     .{ "socket", h.c("blk: { const _sock = std.posix.socket(std.posix.AF.INET, std.posix.SOCK.STREAM, 0) catch break :blk @as(i64, -1); break :blk @as(i64, @intCast(_sock)); }") },
-    .{ "create_connection", genCreateConnection },
+    .{ "create_connection", h.wrap("blk: { const _addr_tuple = ", createConnBody, "@as(i64, -1)") },
     .{ "gethostname", h.c("blk: { var _buf: [std.posix.HOST_NAME_MAX]u8 = undefined; const _result = std.posix.gethostname(&_buf); if (_result) |_name| { break :blk __global_allocator.dupe(u8, _name) catch \"\"; } else |_| break :blk \"\"; }") },
     .{ "getfqdn", h.c("blk: { var _buf: [std.posix.HOST_NAME_MAX]u8 = undefined; const _result = std.posix.gethostname(&_buf); if (_result) |_name| { break :blk __global_allocator.dupe(u8, _name) catch \"\"; } else |_| break :blk \"\"; }") },
-    .{ "inet_aton", genInetAton }, .{ "inet_ntoa", genInetNtoa },
-    .{ "htons", genByteSwap("@as(i64, @intCast(", "u16", "nativeToBig") },
-    .{ "htonl", genByteSwap("@as(i64, @intCast(", "u32", "nativeToBig") },
-    .{ "ntohs", genByteSwap("@as(i64, @intCast(", "u16", "bigToNative") },
-    .{ "ntohl", genByteSwap("@as(i64, @intCast(", "u32", "bigToNative") },
+    .{ "inet_aton", h.wrap("blk: { const _ip_str = ", inetAtonBody, "\"\"") },
+    .{ "inet_ntoa", h.wrap("blk: { const _packed = ", inetNtoaBody, "\"0.0.0.0\"") },
+    .{ "htons", h.wrap("@as(i64, @intCast(std.mem.nativeToBig(u16, @intCast(", "))))", "0") },
+    .{ "htonl", h.wrap("@as(i64, @intCast(std.mem.nativeToBig(u32, @intCast(", "))))", "0") },
+    .{ "ntohs", h.wrap("@as(i64, @intCast(std.mem.bigToNative(u16, @intCast(", "))))", "0") },
+    .{ "ntohl", h.wrap("@as(i64, @intCast(std.mem.bigToNative(u32, @intCast(", "))))", "0") },
     .{ "setdefaulttimeout", h.c("{}") }, .{ "getdefaulttimeout", h.c("null") },
 });
-
-fn genCreateConnection(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) return;
-    try self.emit("blk: { const _addr_tuple = "); try self.genExpr(args[0]);
-    try self.emit("; const _host = _addr_tuple.@\"0\"; const _port = _addr_tuple.@\"1\"; const _sock = std.posix.socket(std.posix.AF.INET, std.posix.SOCK.STREAM, 0) catch break :blk @as(i64, -1); var _addr: std.posix.sockaddr.in = .{ .family = std.posix.AF.INET, .port = std.mem.nativeToBig(u16, @intCast(_port)), .addr = blk2: { if (std.mem.eql(u8, _host, \"localhost\") or std.mem.eql(u8, _host, \"127.0.0.1\")) { break :blk2 .{ .s_addr = std.mem.nativeToBig(u32, 0x7f000001) }; } else { break :blk2 .{ .s_addr = 0 }; } }, .zero = [_]u8{0} ** 8 }; std.posix.connect(_sock, @ptrCast(&_addr), @sizeOf(@TypeOf(_addr))) catch break :blk @as(i64, -1); break :blk @as(i64, @intCast(_sock)); }");
-}
-fn genInetAton(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) return;
-    try self.emit("blk: { const _ip_str = "); try self.genExpr(args[0]);
-    try self.emit("; var _parts: [4]u8 = undefined; var _iter = std.mem.splitScalar(u8, _ip_str, '.'); var _i: usize = 0; while (_iter.next()) |_part| : (_i += 1) { if (_i >= 4) break; _parts[_i] = std.fmt.parseInt(u8, _part, 10) catch 0; } break :blk __global_allocator.dupe(u8, &_parts) catch \"\"; }");
-}
-fn genInetNtoa(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) return;
-    try self.emit("blk: { const _packed = "); try self.genExpr(args[0]);
-    try self.emit("; if (_packed.len < 4) break :blk \"0.0.0.0\"; var _buf: [16]u8 = undefined; const _len = std.fmt.bufPrint(&_buf, \"{d}.{d}.{d}.{d}\", .{ _packed[0], _packed[1], _packed[2], _packed[3] }) catch break :blk \"0.0.0.0\"; break :blk __global_allocator.dupe(u8, _len) catch \"0.0.0.0\"; }");
-}
--- src/codegen/native/zipfile_mod.zig
+++ src/codegen/native/zipfile_mod.zig
@@ -1,32 +1,16 @@
 /// Python zipfile module - ZIP archive handling
 const std = @import("std");
-const ast = @import("ast");
 const h = @import("mod_helper.zig");
-const CodegenError = h.CodegenError;
-const NativeCodegen = h.NativeCodegen;
+
+const zipFileStruct = "; _ = _mode; break :blk struct { path: []const u8, files: std.ArrayList([]const u8), pub fn init(p: []const u8) @This() { return @This(){ .path = p, .files = .{} }; } pub fn namelist(__self: *@This()) [][]const u8 { return __self.files.items; } pub fn read(__self: *@This(), name: []const u8) []const u8 { _ = __self; _ = name; return \"\"; } pub fn write(__self: *@This(), name: []const u8, data: []const u8) void { _ = data; __self.files.append(__global_allocator, name) catch {}; } pub fn writestr(__self: *@This(), name: []const u8, data: []const u8) void { __self.write(name, data); } pub fn extractall(__self: *@This(), path: ?[]const u8) void { _ = __self; _ = path; } pub fn extract(__self: *@This(), member: []const u8, path: ?[]const u8) []const u8 { _ = __self; _ = path; return member; } pub fn close(__self: *@This()) void { _ = __self; } pub fn __enter__(__self: *@This()) *@This() { return __self; } pub fn __exit__(__self: *@This(), _: anytype) void { __self.close(); } }.init(_path); }";
+const isZipBody = "; const file = std.fs.cwd().openFile(_path, .{}) catch break :blk false; defer file.close(); var buf: [4]u8 = undefined; _ = file.read(&buf) catch break :blk false; break :blk std.mem.eql(u8, buf[0..4], \"PK\\x03\\x04\"); }";
+const zipInfoStruct = "struct { filename: []const u8, compress_size: i64 = 0, file_size: i64 = 0, compress_type: i64 = 0, date_time: struct { year: i64, month: i64, day: i64, hour: i64, minute: i64, second: i64 } = .{ .year = 1980, .month = 1, .day = 1, .hour = 0, .minute = 0, .second = 0 } }{ .filename = ";
 
 pub const Funcs = std.StaticStringMap(h.H).initComptime(.{
-    .{ "ZipFile", genZipFile }, .{ "is_zipfile", genIsZipfile }, .{ "ZipInfo", genZipInfo },
+    .{ "ZipFile", h.wrap2("blk: { const _path = ", "; const _mode: []const u8 = ", zipFileStruct, "undefined") },
+    .{ "is_zipfile", h.wrap("blk: { const _path = ", isZipBody, "false") },
+    .{ "ZipInfo", h.wrap(zipInfoStruct, " }", "struct { filename: []const u8 = \"\", compress_size: i64 = 0, file_size: i64 = 0 }{}") },
     .{ "ZIP_STORED", h.I64(0) }, .{ "ZIP_DEFLATED", h.I64(8) },
     .{ "ZIP_BZIP2", h.I64(12) }, .{ "ZIP_LZMA", h.I64(14) },
     .{ "BadZipFile", h.c("\"BadZipFile\"") }, .{ "LargeZipFile", h.c("\"LargeZipFile\"") },
 });
-
-fn genZipFile(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) return;
-    try self.emit("blk: { const _path = "); try self.genExpr(args[0]); try self.emit("; const _mode: []const u8 = ");
-    if (args.len > 1) { try self.genExpr(args[1]); } else { try self.emit("\"r\""); }
-    try self.emit("; _ = _mode; break :blk struct { path: []const u8, files: std.ArrayList([]const u8), pub fn init(p: []const u8) @This() { return @This(){ .path = p, .files = .{} }; } pub fn namelist(__self: *@This()) [][]const u8 { return __self.files.items; } pub fn read(__self: *@This(), name: []const u8) []const u8 { _ = __self; _ = name; return \"\"; } pub fn write(__self: *@This(), name: []const u8, data: []const u8) void { _ = data; __self.files.append(__global_allocator, name) catch {}; } pub fn writestr(__self: *@This(), name: []const u8, data: []const u8) void { __self.write(name, data); } pub fn extractall(__self: *@This(), path: ?[]const u8) void { _ = __self; _ = path; } pub fn extract(__self: *@This(), member: []const u8, path: ?[]const u8) []const u8 { _ = __self; _ = path; return member; } pub fn close(__self: *@This()) void { _ = __self; } pub fn __enter__(__self: *@This()) *@This() { return __self; } pub fn __exit__(__self: *@This(), _: anytype) void { __self.close(); } }.init(_path); }");
-}
-
-fn genIsZipfile(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) return;
-    try self.emit("blk: { const _path = "); try self.genExpr(args[0]); try self.emit("; const file = std.fs.cwd().openFile(_path, .{}) catch break :blk false; defer file.close(); var buf: [4]u8 = undefined; _ = file.read(&buf) catch break :blk false; break :blk std.mem.eql(u8, buf[0..4], \"PK\\x03\\x04\"); }");
-}
-
-fn genZipInfo(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) { try self.emit("struct { filename: []const u8 = \"\", compress_size: i64 = 0, file_size: i64 = 0 }{}"); return; }
-    try self.emit("struct { filename: []const u8, compress_size: i64 = 0, file_size: i64 = 0, compress_type: i64 = 0, date_time: struct { year: i64, month: i64, day: i64, hour: i64, minute: i64, second: i64 } = .{ .year = 1980, .month = 1, .day = 1, .hour = 0, .minute = 0, .second = 0 } }{ .filename = ");
-    try self.genExpr(args[0]);
-    try self.emit(" }");
-}
