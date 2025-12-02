07bf7c888b34df946a3b02d38a54a7c38e2a8dad
teamchong <25894545+teamchong@users.noreply.github.com>
2025-12-02 11:51:18 -0500

    refactor: Replace genEscape/genUnescape with h.wrap() in html_mod
    Removed unused ast import and simplified module.

--- src/codegen/native/html_mod.zig
+++ src/codegen/native/html_mod.zig
@@ -1,24 +1,10 @@
 /// Python html module - HTML entity encoding/decoding
 const std = @import("std");
-const ast = @import("ast");
 const h = @import("mod_helper.zig");
-const CodegenError = h.CodegenError;
-const NativeCodegen = h.NativeCodegen;
+
+pub const genEscape = h.wrap("html_escape_blk: { const _s = ", "; var _result: std.ArrayList(u8) = .{}; for (_s) |c| { switch (c) { '&' => _result.appendSlice(__global_allocator, \"&amp;\") catch {}, '<' => _result.appendSlice(__global_allocator, \"&lt;\") catch {}, '>' => _result.appendSlice(__global_allocator, \"&gt;\") catch {}, '\"' => _result.appendSlice(__global_allocator, \"&quot;\") catch {}, '\\'' => _result.appendSlice(__global_allocator, \"&#x27;\") catch {}, else => _result.append(__global_allocator, c) catch {}, } } break :html_escape_blk _result.items; }", "\"\"");
+pub const genUnescape = h.wrap("html_unescape_blk: { const _s = ", "; var _result: std.ArrayList(u8) = .{}; var _i: usize = 0; while (_i < _s.len) { if (_s[_i] == '&') { if (_i + 4 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 4], \"&lt;\")) { _result.append(__global_allocator, '<') catch {}; _i += 4; continue; } if (_i + 4 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 4], \"&gt;\")) { _result.append(__global_allocator, '>') catch {}; _i += 4; continue; } if (_i + 5 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 5], \"&amp;\")) { _result.append(__global_allocator, '&') catch {}; _i += 5; continue; } if (_i + 6 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 6], \"&quot;\")) { _result.append(__global_allocator, '\"') catch {}; _i += 6; continue; } if (_i + 6 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 6], \"&#x27;\")) { _result.append(__global_allocator, '\\'') catch {}; _i += 6; continue; } if (_i + 6 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 6], \"&apos;\")) { _result.append(__global_allocator, '\\'') catch {}; _i += 6; continue; } } _result.append(__global_allocator, _s[_i]) catch {}; _i += 1; } break :html_unescape_blk _result.items; }", "\"\"");
 
 pub const Funcs = std.StaticStringMap(h.H).initComptime(.{
     .{ "escape", genEscape }, .{ "unescape", genUnescape },
 });
-
-pub fn genEscape(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) return;
-    try self.emit("html_escape_blk: { const _s = ");
-    try self.genExpr(args[0]);
-    try self.emit("; var _result: std.ArrayList(u8) = .{}; for (_s) |c| { switch (c) { '&' => _result.appendSlice(__global_allocator, \"&amp;\") catch {}, '<' => _result.appendSlice(__global_allocator, \"&lt;\") catch {}, '>' => _result.appendSlice(__global_allocator, \"&gt;\") catch {}, '\"' => _result.appendSlice(__global_allocator, \"&quot;\") catch {}, '\\'' => _result.appendSlice(__global_allocator, \"&#x27;\") catch {}, else => _result.append(__global_allocator, c) catch {}, } } break :html_escape_blk _result.items; }");
-}
-
-pub fn genUnescape(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len == 0) return;
-    try self.emit("html_unescape_blk: { const _s = ");
-    try self.genExpr(args[0]);
-    try self.emit("; var _result: std.ArrayList(u8) = .{}; var _i: usize = 0; while (_i < _s.len) { if (_s[_i] == '&') { if (_i + 4 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 4], \"&lt;\")) { _result.append(__global_allocator, '<') catch {}; _i += 4; continue; } if (_i + 4 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 4], \"&gt;\")) { _result.append(__global_allocator, '>') catch {}; _i += 4; continue; } if (_i + 5 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 5], \"&amp;\")) { _result.append(__global_allocator, '&') catch {}; _i += 5; continue; } if (_i + 6 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 6], \"&quot;\")) { _result.append(__global_allocator, '\"') catch {}; _i += 6; continue; } if (_i + 6 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 6], \"&#x27;\")) { _result.append(__global_allocator, '\\'') catch {}; _i += 6; continue; } if (_i + 6 <= _s.len and std.mem.eql(u8, _s[_i .. _i + 6], \"&apos;\")) { _result.append(__global_allocator, '\\'') catch {}; _i += 6; continue; } } _result.append(__global_allocator, _s[_i]) catch {}; _i += 1; } break :html_unescape_blk _result.items; }");
-}
