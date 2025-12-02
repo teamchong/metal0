d5bb43fbdc8317ed9412949c751561191face17d
teamchong <25894545+teamchong@users.noreply.github.com>
2025-12-02 11:41:24 -0500

    refactor: Replace custom generators with h.wrap() in _string_mod
    Simplified formatter_field_name_split and formatter_parser to use
    h.wrap() helper instead of custom generator functions.

--- src/codegen/native/_string_mod.zig
+++ src/codegen/native/_string_mod.zig
@@ -1,32 +1,9 @@
 /// Python _string module - Low-level string formatting (internal)
 /// Ported from CPython's Objects/stringlib/unicode_format.h
 const std = @import("std");
-const ast = @import("ast");
 const h = @import("mod_helper.zig");
-const CodegenError = h.CodegenError;
-const NativeCodegen = h.NativeCodegen;
 
 pub const Funcs = std.StaticStringMap(h.H).initComptime(.{
-    .{ "formatter_field_name_split", genFormatterFieldNameSplit },
-    .{ "formatter_parser", genFormatterParser },
+    .{ "formatter_field_name_split", h.wrap("runtime._string.formatterFieldNameSplit(__global_allocator, ", ")", "runtime._string.FieldNameSplitResult{ .first = \"\", .rest = &[_]runtime._string.FieldAccessor{} }") },
+    .{ "formatter_parser", h.wrap("(runtime._string.formatterParser(__global_allocator, ", ") catch &[_]runtime._string.FormatterResult{})", "&[_]runtime._string.FormatterResult{}") },
 });
-
-fn genFormatterFieldNameSplit(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len > 0) {
-        try self.emit("runtime._string.formatterFieldNameSplit(__global_allocator, ");
-        try self.genExpr(args[0]);
-        try self.emit(")");
-    } else {
-        try self.emit("runtime._string.FieldNameSplitResult{ .first = \"\", .rest = &[_]runtime._string.FieldAccessor{} }");
-    }
-}
-
-fn genFormatterParser(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len > 0) {
-        try self.emit("(runtime._string.formatterParser(__global_allocator, ");
-        try self.genExpr(args[0]);
-        try self.emit(") catch &[_]runtime._string.FormatterResult{})");
-    } else {
-        try self.emit("&[_]runtime._string.FormatterResult{}");
-    }
-}
