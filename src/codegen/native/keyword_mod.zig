5f59ba001650afa81f2fae718a1a7d528464f478
teamchong <25894545+teamchong@users.noreply.github.com>
2025-12-02 11:08:14 -0500

    refactor: Deduplicate keyword lists in keyword_mod

--- src/codegen/native/keyword_mod.zig
+++ src/codegen/native/keyword_mod.zig
@@ -5,39 +5,23 @@ const h = @import("mod_helper.zig");
 const CodegenError = h.CodegenError;
 const NativeCodegen = h.NativeCodegen;
 
+const kwlist = "\"False\", \"None\", \"True\", \"and\", \"as\", \"assert\", \"async\", \"await\", \"break\", \"class\", \"continue\", \"def\", \"del\", \"elif\", \"else\", \"except\", \"finally\", \"for\", \"from\", \"global\", \"if\", \"import\", \"in\", \"is\", \"lambda\", \"nonlocal\", \"not\", \"or\", \"pass\", \"raise\", \"return\", \"try\", \"while\", \"with\", \"yield\"";
+const softkwlist = "\"_\", \"case\", \"match\", \"type\"";
+
 pub const Funcs = std.StaticStringMap(h.H).initComptime(.{
-    .{ "iskeyword", genIskeyword },
-    .{ "kwlist", genKwlist },
-    .{ "softkwlist", genSoftkwlist },
-    .{ "issoftkeyword", genIssoftkeyword },
+    .{ "iskeyword", genIskeyword }, .{ "issoftkeyword", genIssoftkeyword },
+    .{ "kwlist", h.c("metal0_runtime.PyList([]const u8).fromSlice(&[_][]const u8{ " ++ kwlist ++ " })") },
+    .{ "softkwlist", h.c("metal0_runtime.PyList([]const u8).fromSlice(&[_][]const u8{ " ++ softkwlist ++ " })") },
 });
 
 fn genIskeyword(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len > 0) {
-        try self.emit("blk: { const s = ");
-        try self.genExpr(args[0]);
-        try self.emit("; const keywords = [_][]const u8{ \"False\", \"None\", \"True\", \"and\", \"as\", \"assert\", \"async\", \"await\", \"break\", \"class\", \"continue\", \"def\", \"del\", \"elif\", \"else\", \"except\", \"finally\", \"for\", \"from\", \"global\", \"if\", \"import\", \"in\", \"is\", \"lambda\", \"nonlocal\", \"not\", \"or\", \"pass\", \"raise\", \"return\", \"try\", \"while\", \"with\", \"yield\" }; for (keywords) |kw| { if (std.mem.eql(u8, s, kw)) break :blk true; } break :blk false; }");
-    } else {
-        try self.emit("false");
-    }
-}
-
-fn genKwlist(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    _ = args;
-    try self.emit("metal0_runtime.PyList([]const u8).fromSlice(&[_][]const u8{ \"False\", \"None\", \"True\", \"and\", \"as\", \"assert\", \"async\", \"await\", \"break\", \"class\", \"continue\", \"def\", \"del\", \"elif\", \"else\", \"except\", \"finally\", \"for\", \"from\", \"global\", \"if\", \"import\", \"in\", \"is\", \"lambda\", \"nonlocal\", \"not\", \"or\", \"pass\", \"raise\", \"return\", \"try\", \"while\", \"with\", \"yield\" })");
-}
-
-fn genSoftkwlist(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    _ = args;
-    try self.emit("metal0_runtime.PyList([]const u8).fromSlice(&[_][]const u8{ \"_\", \"case\", \"match\", \"type\" })");
+    if (args.len == 0) { try self.emit("false"); return; }
+    try self.emit("blk: { const s = "); try self.genExpr(args[0]);
+    try self.emit("; const keywords = [_][]const u8{ " ++ kwlist ++ " }; for (keywords) |kw| { if (std.mem.eql(u8, s, kw)) break :blk true; } break :blk false; }");
 }
 
 fn genIssoftkeyword(self: *NativeCodegen, args: []ast.Node) CodegenError!void {
-    if (args.len > 0) {
-        try self.emit("blk: { const s = ");
-        try self.genExpr(args[0]);
-        try self.emit("; const softkw = [_][]const u8{ \"_\", \"case\", \"match\", \"type\" }; for (softkw) |kw| { if (std.mem.eql(u8, s, kw)) break :blk true; } break :blk false; }");
-    } else {
-        try self.emit("false");
-    }
+    if (args.len == 0) { try self.emit("false"); return; }
+    try self.emit("blk: { const s = "); try self.genExpr(args[0]);
+    try self.emit("; const softkw = [_][]const u8{ " ++ softkwlist ++ " }; for (softkw) |kw| { if (std.mem.eql(u8, s, kw)) break :blk true; } break :blk false; }");
 }
